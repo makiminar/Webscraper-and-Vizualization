15fdd8421cf87e8d4df5b162f40b2a0f14fa4663
Marketa Minarova <minarma5@fit.cvut.cz>
2021-11-29 16:22:03 +0100

    Set output format and file

--- webscraper/webscraper/settings.py
+++ webscraper/webscraper/settings.py
@@ -14,6 +14,8 @@ BOT_NAME = 'webscraper'
 SPIDER_MODULES = ['webscraper.spiders']
 NEWSPIDER_MODULE = 'webscraper.spiders'
 
+FEED_FORMAT = "csv"
+FEED_URI = "./output_files/realities.csv"
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
 #USER_AGENT = 'webscraper (+http://www.yourdomain.com)'
