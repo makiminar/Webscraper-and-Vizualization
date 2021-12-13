2a36b5ee552da67877f55810f85ad6e2127c4069
Marketa Minarova <minarma5@fit.cvut.cz>
2021-12-13 13:14:30 +0100

    Edit css extraction - add new features and change the way of extracting property

--- webscraper/webscraper/spiders/realitybot.py
+++ webscraper/webscraper/spiders/realitybot.py
@@ -8,7 +8,6 @@ class RealitybotSpider(scrapy.Spider):
     def __init__(self, name=None, **kwargs):
         super().__init__(name)
         self.url = ""
-        self.page = ""
 
     def start_requests(self):
         self.start_urls = ['https://reality.idnes.cz/s/praha',
@@ -28,16 +27,16 @@ class RealitybotSpider(scrapy.Spider):
 
         for url in self.start_urls:
             self.url = url
-            self.page = self.url.split("/")[-1]
             yield scrapy.Request(self.url, self.parse, meta={'region': url.split("/")[-1]})
 
     def parse(self, response, **kwargs):
         reality_page_links = response.css('.c-products__inner a::attr(href)')
-        rqst = response.follow_all(reality_page_links, self.parse_reality, meta=response.meta)
-        yield from rqst
+        yield from response.follow_all(reality_page_links, self.parse_reality, meta=response.meta)
 
-        # next_page_links = response.css('a[class=\'btn paging__item next\']::attr(href)')
-        # yield from response.follow_all(next_page_links, self.parse)
+        next_page_link = response.css('a[class=\'btn paging__item next\']::attr(href)').get()
+        if next_page_link is not None:
+            next_page = 'https://reality.idnes.cz' + next_page_link
+            yield scrapy.Request(next_page, self.parse, meta=response.meta)
 
     def parse_reality(self, response):
         def extract_with_css(query):
@@ -48,18 +47,23 @@ class RealitybotSpider(scrapy.Spider):
             'location': extract_with_css('.row-main .b-detail .b-detail__info::text'),
             'price': extract_with_css('.row-main .b-detail .b-detail__price strong::text'),
             'region': response.meta['region'],
-            'property': extract_with_css(
-                '.row-main div[class = \'b-definition-columns mb-0\'] dl dd:nth-of-type(5)::text'),
-            'building': extract_with_css(
-                '.row-main div[class = \'b-definition-columns mb-0\'] dl dd:nth-of-type(3)::text')
         }
+
         util_area = False
         land_area = False
+        property_info = False
+        building_info = False
+        floors_info = False
+        building_type_info = False
 
         util_area_idx = 0
         land_area_idx = 0
+        property_info_idx = 0
+        building_info_idx = 0
+        floors_info_idx = 0
+        building_type_info_idx = 0
 
-        for x in range(6, 12):
+        for x in range(3, 12):
             if extract_with_css('.row-main div[class = \'b-definition-columns mb-0\'] '
                                 'dl dt:nth-of-type(' + str(x) + ')::text') == 'Užitná plocha':
                 util_area = True
@@ -68,29 +72,67 @@ class RealitybotSpider(scrapy.Spider):
                                 'dl dt:nth-of-type(' + str(x) + ')::text') == 'Plocha pozemku':
                 land_area = True
                 land_area_idx = x
+            if extract_with_css('.row-main div[class = \'b-definition-columns mb-0\'] '
+                                'dl dt:nth-of-type(' + str(x) + ')::text') == 'Konstrukce budovy':
+                building_info = True
+                building_info_idx = x
+            if extract_with_css('.row-main div[class = \'b-definition-columns mb-0\'] '
+                                'dl dt:nth-of-type(' + str(x) + ')::text') == 'Typ komerční nemovitosti':
+                building_type_info = True
+                building_type_info_idx = x
+            if extract_with_css('.row-main div[class = \'b-definition-columns mb-0\'] '
+                                'dl dt:nth-of-type(' + str(x) + ')::text') == 'Podlaží':
+                floors_info = True
+                floors_info_idx = x
+            if extract_with_css('.row-main div[class = \'b-definition-columns mb-0\'] '
+                                'dl dt:nth-of-type(' + str(x) + ')::text') == 'Vlastnictví':
+                property_info = True
+                property_info_idx = x
 
-        if land_area and util_area:
-
-            scraped_info['land_area'] = extract_with_css(
-                '.row-main div[class = \'b-definition-columns mb-0\'] '
-                'dl dd:nth-of-type(' + str(land_area_idx) + ')::text')
-            scraped_info['util_area'] = extract_with_css(
-                '.row-main div[class = \'b-definition-columns mb-0\']'
-                ' dl dd:nth-of-type(' + str(util_area_idx) + ')::text')
-            yield scraped_info
         if land_area:
             scraped_info['land_area'] = extract_with_css(
                 '.row-main div[class = \'b-definition-columns mb-0\'] '
                 'dl dd:nth-of-type(' + str(land_area_idx) + ')::text')
-            scraped_info['util_area'] = ""
-            yield scraped_info
-        if util_area:
+        if not land_area:
             scraped_info['land_area'] = ""
+
+        if util_area:
             scraped_info['util_area'] = extract_with_css(
                 '.row-main div[class = \'b-definition-columns mb-0\']'
                 ' dl dd:nth-of-type(' + str(util_area_idx) + ')::text')
-            yield scraped_info
+        if not util_area:
+            scraped_info['util_area'] = ""
 
-        scraped_info['land_area'] = ""
-        scraped_info['util_area'] = ""
+        if building_info:
+            scraped_info['building'] = extract_with_css(
+                '.row-main div[class = \'b-definition-columns mb-0\']'
+                ' dl dd:nth-of-type(' + str(building_info_idx) + ')::text')
+
+        if not building_info:
+            scraped_info["building"] = ""
+
+        if building_type_info:
+            scraped_info['building_type'] = extract_with_css(
+                '.row-main div[class = \'b-definition-columns mb-0\']'
+                ' dl dd:nth-of-type(' + str(building_type_info_idx) + ')::text')
+
+        if not building_type_info:
+            scraped_info['building_type'] = ""
+
+        if floors_info:
+            scraped_info['floor'] = extract_with_css(
+                '.row-main div[class = \'b-definition-columns mb-0\']'
+                ' dl dd:nth-of-type(' + str(floors_info_idx) + ')::text')
+
+        if not floors_info:
+            scraped_info['floor'] = ""
+
+        if property_info:
+            scraped_info['property'] = extract_with_css(
+                '.row-main div[class = \'b-definition-columns mb-0\']'
+                ' dl dd:nth-of-type(' + str(property_info_idx) + ')::text')
+
+        if not property_info:
+            scraped_info['property'] = ""
+
         yield scraped_info
