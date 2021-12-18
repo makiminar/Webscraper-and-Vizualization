b061ca6c2c695341f1c040e41ad54506d1e16960
Marketa Minarova <minarma5@fit.cvut.cz>
2021-12-18 11:11:58 +0100

    Divide code to functions

--- webscraper/vizualization/clean_data.py
+++ webscraper/vizualization/clean_data.py
@@ -1,6 +1,9 @@
 import pandas as pd
 import numpy as np
 
+file_name = '/home/marketa/Projects/PyCharmProjects/PYT/semestral_work/webscraper/vizualization/cleaned_data' \
+            '/realities.csv '
+
 
 def calculate_price(price, area):
     if price is None or area is None:
@@ -8,45 +11,81 @@ def calculate_price(price, area):
     return price / area
 
 
-df = pd.read_csv('../webscraper/output_files/realities.csv')
+def load_dataset(file_path: str) -> pd.DataFrame:
+    return pd.read_csv(file_path)
 
-df['payment'] = np.where(df.price.str.contains("vyžádání"), "unknown",
-                         np.where(df.price.str.contains("měsíc"), "monthly",
-                                  np.where(df.price.str.endswith("Kč"), "total",
-                                           "unknown")))
 
-df.loc[df.payment == "unknown", "price"] = np.NAN
-df.loc[df.payment == "monthly", "price"] = df.loc[df.payment == "monthly", "price"].map(lambda x: x.rstrip(' Kč/měsíc'))
-df.loc[df.payment == "total", "price"] = df.loc[df.payment == "total", "price"].map(lambda x: x.rstrip(' Kč'))
+def create_payment_column(data: pd.DataFrame) -> pd.DataFrame:
+    data['payment'] = np.where(data.price.str.contains("vyžádání"), "unknown",
+                               np.where(data.price.str.contains("měsíc"), "monthly",
+                                        np.where(data.price.str.endswith("Kč"), "total",
+                                                 "unknown")))
+    return data
 
-df.loc[df["util_area"].notnull(), "util_area"] = df.loc[df["util_area"].notnull(), "util_area"].map(
-    lambda x: x.rstrip(' m'))
 
-df["price"] = df["price"].str.split(' ')
-df["price"] = df["price"].str.join('')
-df["price"] = df["price"].str.replace(",", ".")
+def strip_text_from_payment(data: pd.DataFrame) -> pd.DataFrame:
+    data.loc[data.payment == "unknown", "price"] = np.NAN
+    data.loc[data.payment == "monthly", "price"] = data.loc[data.payment == "monthly", "price"].map(
+        lambda x: x.rstrip(' Kč/měsíc'))
+    data.loc[data.payment == "total", "price"] = data.loc[data.payment == "total", "price"].map(
+        lambda x: x.rstrip(' Kč'))
+    return data
 
-df["has_property"] = df["land_area"].apply(lambda x: pd.notnull(x))
 
-df["property"] = df["property"].replace(np.NaN, "neznámý")
-df["building_type"] = df["building_type"].replace(np.NaN, "neznámý")
+def strip_text_from_util_area(data: pd.DataFrame) -> pd.DataFrame:
+    data.loc[data["util_area"].notnull(), "util_area"] = data.loc[data["util_area"].notnull(), "util_area"].map(
+        lambda x: x.rstrip(' m'))
+    return data
 
-df["title"] = df["title"].str.split("+")
-df["number_of_rooms"] = np.NaN
 
-# extrahování informace o počtu místností ze sloupce 'title'
-for z in df.index:
-    if len(df.loc[z, "title"]) >= 2:
-        if df.loc[z, "title"][0][-1].isnumeric():
-            df.loc[z, "number_of_rooms"] = float(df.loc[z, "title"][0][-1])
-            if df.loc[z, "title"][1][0].isnumeric():
-                df.loc[z, "number_of_rooms"] += float(df.loc[z, "title"][1][0])
-            if df.loc[z, "title"][1][0] == 'k':
-                df.loc[z, "number_of_rooms"] += float(0.5)
+def make_int_from_price(data: pd.DataFrame) -> pd.DataFrame:
+    data["price"] = data["price"].str.split(' ')
+    data["price"] = data["price"].str.join('')
+    data["price"] = data["price"].str.replace(",", ".")
+    data["price"] = pd.to_numeric(data["price"])
+    return data
 
-df["title"] = df["title"].str.join(' ')
 
-print(df)
-df.to_csv('./cleaned_data/realities.csv')
+def create_has_property_column(data: pd.DataFrame) -> pd.DataFrame:
+    data["has_property"] = data["land_area"].apply(lambda x: pd.notnull(x))
+    return data
 
+
+def replace_nan(data: pd.DataFrame) -> pd.DataFrame:
+    data["property"] = data["property"].replace(np.NaN, "neznámý")
+    data["building_type"] = data["building_type"].replace(np.NaN, "neznámý")
+    return data
+
+
+def extract_number_of_rooms_from_title(data: pd.DataFrame) -> pd.DataFrame:
+    data["title"] = data["title"].str.split("+")
+    data["number_of_rooms"] = np.NaN
+    for z in data.index:
+        if len(data.loc[z, "title"]) >= 2:
+            if data.loc[z, "title"][0][-1].isnumeric():
+                data.loc[z, "number_of_rooms"] = float(data.loc[z, "title"][0][-1])
+                if data.loc[z, "title"][1][0].isnumeric():
+                    data.loc[z, "number_of_rooms"] += float(data.loc[z, "title"][1][0])
+                if data.loc[z, "title"][1][0] == 'k':
+                    data.loc[z, "number_of_rooms"] += float(0.5)
+
+    data["title"] = data["title"].str.join(' ')
+    return data
+
+
+def clean_data():
+    df = load_dataset('/home/marketa/Projects/PyCharmProjects/PYT/semestral_work/webscraper/webscraper/output_files'
+                      '/realities.csv')
+    df = create_payment_column(df)
+    df = strip_text_from_payment(df)
+    df = strip_text_from_util_area(df)
+    df = make_int_from_price(df)
+    df = create_has_property_column(df)
+    df = extract_number_of_rooms_from_title(df)
+
+    # noinspection PyTypeChecker
+    df.to_csv(file_name, sep=',', encoding='utf-8')
+
+
+clean_data()
 # title, location, price, util_area, land_area, property, building, payment
